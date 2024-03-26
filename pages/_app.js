9674a32293fae74c07ed672c4608ae3635e6cdfc
Anupam <khamrai.eren.3@gmail.com>
2024-03-26 11:10:33 +0530

    Final 1

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Layout>
+      <Head>
+        <title>Rohit Painter</title>
+        <link rel="apple-touch-icon" href="paint.png" />
+        <link rel="icon" href="paint.png" />
+      </Head>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
